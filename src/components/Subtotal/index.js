577d064f826efc183ec7eb9f94b1b4562ad51014
Adan Palacios <ajpalacios.sist@gmail.com>
2020-12-20 18:53:02 +0000

    remove pakcage lock

--- src/components/Subtotal/index.js
+++ src/components/Subtotal/index.js
@@ -7,12 +7,12 @@ import './Subtotal.css';
 function Subtotal() {
     return (
         <div className="subtotal">
-            <CurrencyFormat 
+            <CurrencyFormat
                 renderText={(value) => (
                     <>
                         <p>Subtotal(0 items): <strong>0</strong></p>
                         <small className="subtotal_gift">
-                            <input type="checkbox"/>
+                            <input type="checkbox" />
                             This order contains a gift
                         </small>
                     </>
